53d73c8a6c1538fe260990eb43b4a75bacde831c
Maksim Logvinov <ragdor1996@gmail.com>
2018-09-19 11:20:55 +0300

    First release

---
+++ javascript.js
@@ -0,0 +1,42 @@
+function openNav() {
+    document.getElementById("SideNavMenu").style.marginLeft = "0";
+    document.getElementById("SideNavMenu").style.width = "100vw";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)t";
+}
+
+function closeNav() {
+    document.getElementById("SideNavMenu").style.marginLeft = "-100vw";
+    document.body.style.backgroundColor = "white";
+}
+
+function playPause(btn,vid){
+    var vid = document.getElementById(vid);
+    if(vid.paused){
+        vid.play();
+        btn.innerHTML = "<i class=\"fas fa-pause\"></i>";
+    } else {
+        vid.pause();
+        btn.innerHTML = "<i class=\"fas fa-play\"></i>";
+    }
+}
+function showContentExchanger() {
+    document.getElementById("descriptionHeatExchangerNext").style.display = "block";
+    document.getElementById("expandDataLinkExchanger").style.display = "none";
+}
+function hideContentExchanger() {
+    document.getElementById("descriptionHeatExchangerNext").style.display = "none";
+    document.getElementById("expandDataLinkExchanger").style.display = "inline";
+}
+
+function showContent() {
+    document.getElementById("descriptionHeatExchanger").style.display = "block";
+    document.getElementById("expandDataLink").style.display = "none";
+}
+function hideContent() {
+    document.getElementById("descriptionHeatExchanger").style.display = "none";
+    document.getElementById("expandDataLink").style.display = "inline";
+}
+
+$(function () {
+    $('[data-toggle="popover"]').popover()
+})
